2aa5f881038e71e75fc79e35ddc5589ca255a26f
KPTN <kopetan@devnullmail.com>
2023-07-04 12:18:40 +0700

    Create json_code.js

---
+++ json_code.js
@@ -0,0 +1,81 @@
+var json_ = [
+	[
+		/* Name of JSON (Just Orientation) */
+		"StartPage"
+	],
+	[
+		[
+			/* List per Row */
+			3,
+			2,
+			1
+		],
+		[
+			/* Names */
+			"uno", "two", "san", "quad", "mama", "yuga"
+		],
+		[
+			/* Sizes */
+			4,
+			4,
+			4,
+			4,
+			4,
+			4
+		]
+	],
+	[
+		/* Lists */
+		[
+			/* List 00 */
+			["Nasm Doc", "https://www.nasm.us/doc/nasmdocb.html"],
+			["MIPS ASM", "https://chortle.ccsu.edu/AssemblyTutorial/index.html"],
+			["X86 Opcode", "http://ref.x86asm.net/coder64.html"],
+			["Syscall w3chall", "https://syscalls.w3challs.com/"],
+			["ASM-Basic", "https://www.tutorialspoint.com/assembly_programming/"]
+		],
+		[
+			/* List 01 */
+			["Gmail", "https://www.google.com/intl/en/gmail/about/"],
+			["Notion", "https://www.notion.so/"],
+			["Dillinger", "https://dillinger.io/"],
+			["Draw.IO", "https://app.diagrams.net/"]
+		],
+		[
+			/* List 02 */
+			["DEV-docs", "https://devdocs.io/settings"],
+			["[W3]schools", "https://www.w3schools.com/"],
+			["CPP", "http://www.cplusplus.com/reference/"],
+			["Octave", "https://octave.org/doc/v6.1.0/"],
+			["Python", "https://docs.python.org/3/tutorial/datastructures.html"]
+		],
+		[
+			/* List 03 */
+			["Unicode List", "https://www.isthisthingon.org/unicode/?page=0F&subpage=D"],
+			["Fancy-Text", "https://nickfinder.com/fancy-text"],
+			["Ascii Art", "https://ascii.co.uk/art"],
+			["Cryptii", "https://cryptii.com/"],
+			["(J)emoticons", "https://www.jemoticons.com/en"]
+		],
+		[
+			/* List 04 */
+			["Twitter", "https://twitter.com/twitter"],
+			["Instagram", "https://www.instagram.com/instagram"],
+			["Youtube", "https://www.youtube.com/"],
+			["Facebook", "https://www.facebook.com/GitHub/"],
+			["Discord", "https://discord.com/login"],
+			["Reddit", "https://www.reddit.com/"],
+			["GitHub", "https://github.com/explore"]
+		],
+		[
+			/* List 05 */
+			["JSON Editor", "https://jsoneditoronline.org/"],
+			["FileFormat", "https://www.fileformat.info/index.htm"],
+			["RosettaCode", "https://rosettacode.org/wiki/Category:Octave"],
+			["Science General", "https://www.cs.utah.edu/~germain/PPS/Topics/"],
+			["Desmos", "https://www.desmos.com/calculator"],
+			["FlatIcon", "https://www.flaticon.com/"],
+			["Instructables", "https://www.instructables.com/"]
+		]
+	]
+]
